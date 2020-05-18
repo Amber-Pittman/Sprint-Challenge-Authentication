7892a6f427cc2f23a2c38ae92da26fd4b9fb2d8d
Amber-Pittman <amber@amberpittman.com>
2020-05-17 20:22:00 -0400

    Tweaks

--- database/migrations/20180810082348_users.js
+++ database/migrations/20180810082348_users.js
@@ -1,15 +1,15 @@
-exports.up = async function(knex) {
-  await knex.schema.createTable("users", (table) => {
-    table.increments();
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users => {
+    users.increments();
 
-    table
+    users
       .string('username', 255)
       .notNullable()
       .unique();
-      table.string('password', 255).notNullable();
+    users.string('password', 255).notNullable();
   });
 };
 
-exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists("users");
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
 };
